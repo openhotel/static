491888060c87b90684862617791f0b95542deae3
Alberto Quiles Boix <alberto.quil3s@gmail.com>
2025-02-21 01:34:43 +0100

    feat: only allow auth admins - fix #19

--- app/client/src/shared/hooks/useAppSession.tsx
+++ app/client/src/shared/hooks/useAppSession.tsx
@@ -1,8 +1,22 @@
-import React, { ReactNode, useContext, useEffect, useState } from "react";
-import { useCookies } from "shared/hooks/useCookies";
+import React, {
+  ReactNode,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { useApi, useCookies } from "shared/hooks";
+
+type User = {
+  accountId: string;
+  admin: boolean;
+  languages: string[];
+  username: string;
+};
 
 type AppSessionState = {
-  user: any;
+  getHeaders: () => Record<string, string>;
+  user?: User;
 };
 
 const AppSessionContext = React.createContext<AppSessionState>(undefined);
@@ -15,12 +29,22 @@ export const AppSessionProvider: React.FunctionComponent<ProviderProps> = ({
   children,
 }) => {
   const { get, set } = useCookies();
+  const { fetch } = useApi();
+
+  const [isLogged, setIsLogged] = useState(false);
+  const [user, setUser] = useState<User | undefined>(undefined);
 
   const params = new URLSearchParams(window.location.hash.replace("#", "?"));
   const $accountId = params.get("accountId");
   const $accountToken = params.get("accountToken");
 
-  const [user, setUser] = useState(null);
+  const getHeaders = useCallback(
+    () => ({
+      "account-id": $accountId ?? get("account-id"),
+      "account-token": $accountToken ?? get("account-token"),
+    }),
+    [$accountId, $accountToken, get],
+  );
 
   useEffect(() => {
     const accountId = $accountId ?? get("account-id");
@@ -33,35 +57,40 @@ export const AppSessionProvider: React.FunctionComponent<ProviderProps> = ({
     (async () => {
       const {
         data: { enabled },
-      } = await fetch("/api/auth").then((response) => response.json());
+      } = await fetch({
+        pathname: "auth",
+      });
 
       if (!enabled) return;
 
-      if (accountId) {
-        const { status, data } = await fetch(
-          `/api/auth/user?accountId=${accountId}&accountToken=${accountToken}`,
-        ).then((response) => response.json());
+      if (accountId && accountToken) {
+        const { status, data } = await fetch({
+          pathname: `auth/user`,
+          headers: getHeaders(),
+        });
 
         if (status === 200) {
-          setUser(data);
           set("account-id", accountId, 1);
           set("account-token", accountToken, 1);
+          setIsLogged(true);
+          setUser(data);
           return;
         }
       }
-
-      fetch("/api/auth/redirect")
-        .then((response) => response.json())
-        .then(({ data }) => (window.location.href = data.url));
+      const { data } = await fetch({
+        pathname: "auth/redirect",
+      });
+      window.location.href = data.url;
     })();
-  }, [setUser]);
+  }, [setIsLogged, setUser]);
 
   return (
     <AppSessionContext.Provider
       value={{
+        getHeaders,
         user,
       }}
-      children={children}
+      children={isLogged ? children : null}
     />
   );
 };
--- app/server/src/system/accounts.ts
+++ app/server/src/system/accounts.ts
@@ -1,19 +1,15 @@
-import { RequestType, RequestMethod } from "@oh/utils";
 import { System } from "system/main.ts";
-import { RequestKind } from "shared/enums/main.ts";
 
-export const userRequest: RequestType = {
-  method: RequestMethod.GET,
-  pathname: "/user",
-  kind: RequestKind.ACCOUNT,
-  func: async (request: Request) => {
+export const accounts = () => {
+  const getData = async (request: Request) => {
     const accountId = request.headers.get("account-id");
     const accountToken = request.headers.get("account-token");
+
     const {
       auth: { url: authUrl, appToken },
     } = System.getConfig();
 
-    const data = await fetch(`${authUrl}/api/v3/user/@me`, {
+    const { status, data } = await fetch(`${authUrl}/api/v3/user/@me`, {
       headers: {
         "app-token": appToken,
         "account-id": accountId,
@@ -21,8 +17,12 @@ export const userRequest: RequestType = {
       },
     }).then((response) => response.json());
 
-    return Response.json(data, {
-      status: data.status,
-    });
-  },
+    if (status !== 200) return null;
+
+    return data;
+  };
+
+  return {
+    getData,
+  };
 };
